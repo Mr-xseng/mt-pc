1b9529d600e20b5dcd62bd4391870c651b4fcce4
Mr-xseng <18807643549@163.com>
2018-11-13 23:25:43 +0800

    vue-ssr initialized

---
+++ components/public/header/topbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-row
+    :gutter="0"
+    class="m-header">
+    <el-col :span="5">
+      <head-geo/>
+    </el-col>
+    <el-col :span="5">
+      <head-user/>
+    </el-col>
+    <el-col :span="14">
+      <nav-bar/>
+    </el-col>
+  </el-row>
+</template>
+<script>
+import HeadGeo from './geo'
+import HeadUser from './user'
+import NavBar from './nav'
+export default {
+  components: {
+    HeadGeo,
+    HeadUser,
+    NavBar
+  }
+}
+</script>
+<style lang="scss">
+  @import "../../../assets/css/public/layout.scss";
+  @import "../../../assets/css/public/header/index.scss";
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <el-container class="layout-default">
+    <el-header height="197px">
+      <my-header/>
+    </el-header>
+    <el-main>
+      <nuxt/>
+    </el-main>
+    <el-footer height="100%">
+      footer
+    </el-footer>
+  </el-container>
+</template>
+<script>
+import MyHeader from '../components/public/header/index.vue'
+export default {
+  components: {
+    MyHeader
+  }
+}
+</script>
+
